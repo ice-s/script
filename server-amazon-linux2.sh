e2e54fcc05e65f4d545b6ac35dd747b2e4c7a74a
Son Nguyen Truong <son.nguyen.truong@ntq-solution.com.vn>
2019-11-26 11:13:16 +0700

    update

--- server-amazon-linux2.sh
+++ server-amazon-linux2.sh
@@ -39,23 +39,28 @@ else
     exit 1;
 fi
 
-
-#yum update -y
-#echo '>> Setting timezone to America/New_York and installing NTP'
-#timedatectl set-timezone Asia/Ho_Chi_Minh
-#yum install -y ntp
-#systemctl start ntpd
-#systemctl enable ntpd
-#
-#echo '>> Configuring swap'
-#fallocate -l 1G /swapfile
-#chmod 600 /swapfile
-#mkswap /swapfile
-#swapon /swapfile
-#sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
-
 echo ">> OS : $OS"
 echo ">> OS Version : $OS_VER"
+
+if [ $OS_VER == 'CentOS6' ] || [ $OS_VER == 'CentOS7' ] || [ $OS_VER == 'CentOS8' ] ;
+then
+  yum update -y
+  echo '>> Setting timezone to America/New_York and installing NTP'
+  timedatectl set-timezone Asia/Ho_Chi_Minh
+  yum install -y ntp
+  systemctl start ntpd
+  systemctl enable ntpd
+
+  isSwapOn=$(swapon -s | tail -1)
+  if [[ "$isSwapOn" == "" ]]; then
+    echo '>> Configuring swap'
+    fallocate -l 1G /swapfile
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+    sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+  fi
+fi
 
 if [ $OS == 'Amazon Linux AMI' ];
 then
