7c72e29d987ac2a823bfa58f7c0c90b4c3e5d8ba
Son Nguyen Truong <son.nguyen.truong@ntq-solution.com.vn>
2019-11-26 10:44:43 +0700

    update

--- server-amazon-linux2.sh
+++ server-amazon-linux2.sh
@@ -11,9 +11,7 @@ fi
 # Check OS
 echo "Check Your OS"
 if cat /etc/*release | grep CentOS > /dev/null 2>&1; then
-
     OS="CentOS"
-
     if [ $(rpm --eval '%{centos_ver}') == '6' ] ;then
         OS_VER="CentOS6"
     elif [ $(rpm --eval '%{centos_ver}') == '7' ] ;then
@@ -22,9 +20,7 @@ if cat /etc/*release | grep CentOS > /dev/null 2>&1; then
         OS_VER="CentOS8"
     fi
 elif cat /etc/*release | grep ^NAME | grep Ubuntu > /dev/null 2>&1; then
-
     OS="Ubuntu"
-
     if [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'trusty' ] ;then
         OS_VER="Ubuntu14"
     elif [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'xenial' ] ;then
@@ -43,37 +39,47 @@ else
     exit 1;
 fi
 
+
+#yum update -y
+#echo '>> Setting timezone to America/New_York and installing NTP'
+#timedatectl set-timezone Asia/Ho_Chi_Minh
+#yum install -y ntp
+#systemctl start ntpd
+#systemctl enable ntpd
+#
+#echo '>> Configuring swap'
+#fallocate -l 1G /swapfile
+#chmod 600 /swapfile
+#mkswap /swapfile
+#swapon /swapfile
+#sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+
 echo ">> OS : $OS"
 echo ">> OS Version : $OS_VER"
 
-exit 1;
+if OS="Amazon Linux AMI" then
+  yum install -y httpd24 php72 php72-mysqlnd
 
-yum update -y
-echo '>> Setting timezone to America/New_York and installing NTP'
+  echo '>> Add your user (in this case, ec2-user) to the apache group.'
+  usermod -a -G apache ec2-user
 
-timedatectl set-timezone Asia/Ho_Chi_Minh
-yum install -y ntp
-systemctl start ntpd
-systemctl enable ntpd
+  echo '>> Change the group ownership of /var/www and its contents to the apache group.'
+  chown -R ec2-user:apache /var/www
+fi
 
-echo '>> Configuring swap'
-fallocate -l 1G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-sh -c 'echo "/swapfile none swap sw 0 0" >> /etc/fstab'
+if OS="Amazon Linux 2" then
+  echo '>> Installing Apache2'
+  yum install -y httpd
+  systemctl start httpd.service
+  systemctl enable httpd.service
 
-echo '>> Installing Apache2'
-yum install -y httpd
-systemctl start httpd.service
-systemctl enable httpd.service
+  echo '>> Installing PHP7.2'
+  #EC2 :  Amazon Linux 2 AMI
+  amazon-linux-extras install -y php7.2
 
-echo '>> Installing PHP7.2'
-#EC2 :  Amazon Linux 2 AMI
-amazon-linux-extras install -y php7.2
+  echo '>> Add your user (in this case, ec2-user) to the apache group.'
+  usermod -a -G apache ec2-user
 
-echo '>> Add your user (in this case, ec2-user) to the apache group.'
-usermod -a -G apache ec2-user
-
-echo '>> Change the group ownership of /var/www and its contents to the apache group.'
-chown -R ec2-user:apache /var/www
+  echo '>> Change the group ownership of /var/www and its contents to the apache group.'
+  chown -R ec2-user:apache /var/www
+fi
